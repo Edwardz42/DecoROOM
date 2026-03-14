7e46f25ac4806d30b98f54a36ce8edd8e9959176
Edward Zhang <z5589190@ad.unsw.edu.au>
2026-03-14 13:33:19 +1100

    changes

--- frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-const scene = new THREE.scene();
-const camera = new THREE.PerspectiveCamera(55, window.innerWidth, window.innerHeight);
